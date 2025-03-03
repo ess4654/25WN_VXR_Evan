b1a7e6d51e244c15be0e287c509ad73933dc5102
Evan S <thejoblesscoder@gmail.com>
2025-03-03 10:19:47 -0700

    Adding work from the weekend

--- Assets/VXR1161/Scripts/Escalator.cs
+++ Assets/VXR1161/Scripts/Escalator.cs
@@ -6,29 +6,41 @@ using UnityEngine;
 [RequireComponent(typeof(MeshRenderer))]
 public class Escalator : MonoBehaviour
 {
-    [SerializeField, Min(0)] private int materialIndex = 1;
+    [SerializeField, Min(0)] private int treadIndex = 1;
+    [SerializeField, Min(0)] private int railingIndex = 3;
     [SerializeField, Min(.01f)] private float speed = 1;
     [SerializeField] private bool reverse;
 
     private MeshRenderer renderer;
-    private MaterialPropertyBlock props;
+    private MaterialPropertyBlock treadProps;
+    private MaterialPropertyBlock railingProps;
     private float offset;
     private const float threshold = .125f;
     private const string property = "_Offset";
 
     private void Awake()
     {
-        props = new MaterialPropertyBlock();
+        treadProps = new MaterialPropertyBlock();
+        railingProps = new MaterialPropertyBlock();
         renderer = GetComponent<MeshRenderer>();
-        renderer.GetPropertyBlock(props, materialIndex);
+        renderer.GetPropertyBlock(treadProps, treadIndex);
+        renderer.GetPropertyBlock(railingProps, railingIndex);
     }
 
     private void Update()
     {
-        props.SetFloat(property, Mathf.Abs(offset));
-        renderer.SetPropertyBlock(props, materialIndex);
+        //update the treads
+        treadProps.SetFloat(property, Mathf.Abs(offset));
+        renderer.SetPropertyBlock(treadProps, treadIndex);
 
-        offset += Time.smoothDeltaTime * speed * (reverse ? -1 : 1);
+        //increase offset
+        var delta = Time.smoothDeltaTime * speed * (reverse ? -1 : 1);
+        offset += delta;
+
+        //update the railing
+        railingProps.SetFloat(property, Mathf.Abs(offset - (delta / 2.0f)));
+        renderer.SetPropertyBlock(railingProps, railingIndex);
+
         if (offset >= threshold || offset < 0)
             offset = reverse ? threshold : 0;
     }
